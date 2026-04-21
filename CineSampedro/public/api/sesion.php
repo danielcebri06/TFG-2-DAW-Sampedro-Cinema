82ee7f6e1906b6cc6c4ef67ab5d834183f321ce1
llanser <laura.landeiraserantes@gmail.com>
2026-04-21 17:37:00 +0200

    Update sesion.php

--- CineSampedro/public/api/sesion.php
+++ CineSampedro/public/api/sesion.php
@@ -1,7 +1,65 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use App\Controlador\ApiSesionController;
+
+$controlador = new ApiSesionController();
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+$id_sesion = filter_input(INPUT_GET, 'id_sesion', FILTER_VALIDATE_INT);
+
+switch ($metodo){
+    case 'GET':
+        if (!$id_sesion){
+            $controlador->obtener($id_sesion);
+        } else {
+            $controlador->listar();
+        }
+        break;
+    
+    case 'POST':
+        $controlador->crear();
+        break;
+    
+    case 'PUT':
+        if(!$id_sesion){
+            $controlador->modificar($id_sesion);
+        } else {
+            http_response_code(400);
+            
+            header('Content-Type: application/json; charset=utf-8');
+            
+            echo json_encode([
+                'mensaje' => 'Id de sesión obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+        
+    case 'DELETE':
+        if (!$id_sesion) {
+            $controlador->eliminar($id_sesion);            
+        } else {
+            http_response_code(400);
+            
+            header('Content-Type: application/json; charset=utf-8');
+            
+            echo json_encode([
+                'mensaje' => 'Id de sesión obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+        
+    default:
+        http_response_code(405);
+            
+        header('Content-Type: application/json; charset=utf-8');
+            
+        echo json_encode([
+            'mensaje' => 'Método no permitido'
+        ], JSON_UNESCAPED_UNICODE);
+        
+        break;    
+}
 
