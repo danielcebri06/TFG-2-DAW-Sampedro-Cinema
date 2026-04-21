4555fecba03e85f4e9dad46f721c37a5f61b6c47
llanser <laura.landeiraserantes@gmail.com>
2026-04-21 20:01:49 +0200

    Update Sala.php

--- CineSampedro/Modelo/Entidades/Sala.php
+++ CineSampedro/Modelo/Entidades/Sala.php
@@ -1,7 +1,33 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+namespace App\Modelo\Entidades;
+
+class Sala {
+    private ?int $id_sala;
+    private int $numero;
+    private int $capacidad;
+    
+    
+    public function __construct(
+            ?int $id_sala,
+            int $numero,
+            int $capacidad
+    ){
+        $this->id_sala = $id_sala;
+        $this->numero = $numero;
+        $this->capacidad = $capacidad;
+    }
+    
+    public function getId_sala(): ?int {
+        return $this->id_sala;
+    }
+
+    public function getNumero(): int {
+        return $this->numero;
+    }
+
+    public function getCapacidad(): int {
+        return $this->capacidad;
+    }    
+}
 
