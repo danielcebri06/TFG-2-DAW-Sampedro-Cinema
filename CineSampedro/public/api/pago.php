d341b5f2f1cfd939bb36874111f9135c7d829ede
llanser <laura.landeiraserantes@gmail.com>
2026-04-21 20:01:52 +0200

    Update pago.php

--- CineSampedro/public/api/pago.php
+++ CineSampedro/public/api/pago.php
@@ -1,7 +1,63 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use App\Controlador\ApiPagoController;
+
+$controlador = new ApiPagoController();
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+$id_pago = filter_input(INPUT_GET, 'id_pago', FILTER_VALIDATE_INT);
+$id_reserva = filter_input(INPUT_GET, 'id_reserva', FILTER_VALIDATE_INT);
+$accion = filter_input(INPUT_GET, 'accion');
+
+switch ($metodo) {
+    case 'GET':
+        if ($id_pago) {
+            $controlador->obtener($id_pago);
+        } elseif ($id_reserva) {
+            $controlador->obtenerPorReserva($id_reserva);
+        } else {
+            $controlador->listar();
+        }
+        break;
+
+    case 'POST':
+        $controlador->crear();
+        break;
+
+    case 'PUT':
+        if ($id_pago && $accion === 'estado') {
+            $controlador->actualizarEstado($id_pago);
+        } elseif ($id_pago) {
+            $controlador->modificar($id_pago);
+        } else {
+            http_response_code(400);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode([
+                'mensaje' => 'Id de pago obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    case 'DELETE':
+        if ($id_pago) {
+            $controlador->eliminar($id_pago);
+        } else {
+            http_response_code(400);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode([
+                'mensaje' => 'Id de pago obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    default:
+        http_response_code(405);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode([
+            'mensaje' => 'Método no permitido'
+        ], JSON_UNESCAPED_UNICODE);
+        break;
+}
 
