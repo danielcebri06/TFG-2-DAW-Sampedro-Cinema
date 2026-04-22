088332f5db5f1f9ff01106da3390d86c0da85de3
llanser <laura.landeiraserantes@gmail.com>
2026-04-22 18:07:40 +0200

    Update ReservaAsiento.php

--- CineSampedro/Modelo/Entidades/ReservaAsiento.php
+++ CineSampedro/Modelo/Entidades/ReservaAsiento.php
@@ -1,7 +1,33 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+namespace App\Modelo\Entidades;
+
+class ReservaAsiento {
+
+    private ?int $id;
+    private int $id_reserva;
+    private int $id_asiento;
+
+    public function __construct(
+        ?int $id,
+        int $id_reserva,
+        int $id_asiento
+    ) {
+        $this->id = $id;
+        $this->id_reserva = $id_reserva;
+        $this->id_asiento = $id_asiento;
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getId_reserva(): int {
+        return $this->id_reserva;
+    }
+
+    public function getId_asiento(): int {
+        return $this->id_asiento;
+    }
+}
 
