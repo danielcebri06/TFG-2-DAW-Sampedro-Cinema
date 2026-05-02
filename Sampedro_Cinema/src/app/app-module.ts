a2b45cdb5402cfa8a520811bee3ea945202e5067
llanser <laura.landeiraserantes@gmail.com>
2026-05-02 11:30:15 +0200

    Update app-module.ts

--- Sampedro_Cinema/src/app/app-module.ts
+++ Sampedro_Cinema/src/app/app-module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
 import { CarteleraComponent } from './cartelera/cartelera';
 import { ReservaEntradasComponent } from './reserva-entradas/reserva-entradas';
 import { TestApiComponent } from './test-api/test-api';
+import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
+import { AdminPeliculas } from './admin/admin-peliculas/admin-peliculas';
+import { AdminSesiones } from './admin/admin-sesiones/admin-sesiones';
+import { AdminSalas } from './admin/admin-salas/admin-salas';
 
 @NgModule({
   declarations: [
     App,
     CarteleraComponent,
     ReservaEntradasComponent,
-    TestApiComponent
+    TestApiComponent,
+    AdminDashboard,
+    AdminPeliculas,
+    AdminSesiones,
+    AdminSalas,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // Permite las peticiones a la API
+    HttpClientModule, // Permite las peticiones a la API
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
