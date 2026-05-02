8d5c17f38eeaf65d58651697c3b4bd81e6ede640
llanser <laura.landeiraserantes@gmail.com>
2026-05-02 11:30:18 +0200

    Create admin-sesiones.ts

---
+++ Sampedro_Cinema/src/app/admin/admin-sesiones/admin-sesiones.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-sesiones',
+  standalone: false,
+  templateUrl: './admin-sesiones.html',
+  styleUrl: './admin-sesiones.css',
+})
+export class AdminSesiones {}
