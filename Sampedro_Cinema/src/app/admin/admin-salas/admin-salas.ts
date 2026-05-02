6d5836f36551f91b408434543c8b6f3136ec8a10
llanser <laura.landeiraserantes@gmail.com>
2026-05-02 11:31:12 +0200

    Create admin-salas.ts

---
+++ Sampedro_Cinema/src/app/admin/admin-salas/admin-salas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-salas',
+  standalone: false,
+  templateUrl: './admin-salas.html',
+  styleUrl: './admin-salas.css',
+})
+export class AdminSalas {}
