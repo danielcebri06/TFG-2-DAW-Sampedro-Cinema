7efdca079fca40359baa08cc0f060a78bd2c8a76
llanser <laura.landeiraserantes@gmail.com>
2026-05-09 14:04:18 +0200

    Update admin-peliculas.ts

--- Sampedro_Cinema/src/app/admin/admin-peliculas/admin-peliculas.ts
+++ Sampedro_Cinema/src/app/admin/admin-peliculas/admin-peliculas.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Admin } from '../../services/admin';
 
 @Component({
@@ -7,13 +7,15 @@ import { Admin } from '../../services/admin';
   templateUrl: './admin-peliculas.html',
   styleUrl: './admin-peliculas.css',
 })
-
-export class AdminPeliculas implements OnInit{
+export class AdminPeliculas implements OnInit {
   peliculas: any[] = [];
   cargando: boolean = true;
   error: string = '';
 
-  constructor(private adminService: Admin) {}
+  constructor(
+    private adminService: Admin,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.cargarPeliculas();
@@ -27,12 +29,38 @@ export class AdminPeliculas implements OnInit{
       next: (datos) => {
         this.peliculas = datos;
         this.cargando = false;
+
+        this.cd.detectChanges();
       },
       error: (error) => {
-        console.error(error);
-        this.error = 'No se pudieron cargar las peliculas.';
+        console.error('Error al cargar películas:', error);
+
+        this.error = 'No se pudieron cargar las películas.';
         this.cargando = false;
+
+        this.cd.detectChanges();
       }
     });
   }
+
+  eliminarPelicula(id: number): void {
+    const confirmar = confirm('¿Seguro que quieres eliminar esta película?');
+
+    if (!confirmar) {
+      return;
+    }
+
+    this.adminService.eliminarPelicula(id).subscribe({
+      next: () => {
+        this.cargarPeliculas();
+      },
+      error: (error) => {
+        console.error('Error al eliminar película:', error);
+        this.error = 'No se pudo eliminar la película.';
+        this.cd.detectChanges();
+      }
+    });
+  }
+  
 }
+
