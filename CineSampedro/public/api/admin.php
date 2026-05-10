74de705cb45a77706e9d35475e2355fb0d62561e
llanser <laura.landeiraserantes@gmail.com>
2026-05-10 11:37:00 +0200

    Update admin.php
    Se añadieron los GET, PUT, POST y DELETE para salas

--- CineSampedro/public/api/admin.php
+++ CineSampedro/public/api/admin.php
@@ -7,6 +7,7 @@ use App\Controlador\ApiAdminController;
 $controlador = new ApiAdminController();
 $metodo = $_SERVER['REQUEST_METHOD'];
 $accion = filter_input(INPUT_GET, 'accion');
+$id_sala = filter_input(INPUT_GET, 'id_sala', FILTER_VALIDATE_INT);
 
 switch ($metodo) {
     case 'GET':
@@ -28,7 +29,11 @@ switch ($metodo) {
                 break;
 
             case 'salas':
-                $controlador->listarSalas();
+                if ($id_sala) {
+                    $controlador->obtenerSala($id_sala);
+                } else {
+                    $controlador->listarSalas();
+                }
                 break;
 
             default:
@@ -41,6 +46,42 @@ switch ($metodo) {
         }
         break;
 
+    case 'POST':
+        if ($accion === 'salas') {
+            $controlador->crearSala();
+        } else {
+            http_response_code(400);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode([
+                'mensaje' => 'Acción administrativa no válida'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    case 'PUT':
+        if ($accion === 'salas' && $id_sala) {
+            $controlador->modificarSala($id_sala);
+        } else {
+            http_response_code(400);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode([
+                'mensaje' => 'Id de sala obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    case 'DELETE':
+        if ($accion === 'salas' && $id_sala) {
+            $controlador->eliminarSala($id_sala);
+        } else {
+            http_response_code(400);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode([
+                'mensaje' => 'Id de sala obligatorio'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
     default:
         http_response_code(405);
         header('Content-Type: application/json; charset=utf-8');
@@ -49,4 +90,3 @@ switch ($metodo) {
         ], JSON_UNESCAPED_UNICODE);
         break;
 }
-
